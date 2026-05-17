a543b25b4e67ff5710aabfab64c72cb53dcb8016
Lotus <hankong.kong@mail.utoronto.ca>
2026-05-16 21:31:11 -0400

    for running

--- infer_wild.py
+++ infer_wild.py
@@ -35,7 +35,16 @@ if torch.cuda.is_available():
 
 print('Loading checkpoint', opts.evaluate)
 checkpoint = torch.load(opts.evaluate, map_location=lambda storage, loc: storage)
-model_backbone.load_state_dict(checkpoint['model_pos'], strict=True)
+state_dict = checkpoint['model_pos']
+
+new_state_dict = {}
+for k, v in state_dict.items():
+    if k.startswith('module.'):
+        new_state_dict[k[7:]] = v
+    else:
+        new_state_dict[k] = v
+
+model_backbone.load_state_dict(new_state_dict, strict=True)
 model_pos = model_backbone
 model_pos.eval()
 testloader_params = {
